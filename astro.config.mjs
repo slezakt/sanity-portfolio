b4f3ccad91e2b807f133f071295c1eb62b468ae2
Tomáš Slezák <info@tomasslezak.cz>
2023-02-22 19:40:57 +0100

    add netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import sanity from 'astro-sanity';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     sanity({
       projectId: '9quhv77c',
