45b06483a3e018119324630aa76443810f74def7
Tomáš Slezák <info@tomasslezak.cz>
2023-02-22 20:01:38 +0100

    env config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 // https://astro.build/config
 import sanity from 'astro-sanity';
 import netlify from '@astrojs/netlify/functions';
+
+const { PROJECT_ID } = loadEnv(
+  import.meta.env.MODE,
+  process.cwd(),
+  ''
+);
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +17,7 @@ export default defineConfig({
   adapter: netlify(),
   integrations: [
     sanity({
-      projectId: '9quhv77c',
+      projectId: PROJECT_ID,
       dataset: 'production',
       apiVersion: 'v2021-03-25',
       useCdn: true,
